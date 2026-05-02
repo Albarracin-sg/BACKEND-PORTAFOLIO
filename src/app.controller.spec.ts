0c91898ddfa3f357b2058dd21ded9e201a488bf3
Albarracin-sg <camilinplin2024@gmail.com>
2026-05-02 05:09:23 -0500

    test: add basic unit and e2e tests to fix CI pipeline

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      // Nota: Si cambiaste el retorno de appService.getHello(), actualizá este test.
+      expect(appController.getHello()).toBeDefined();
+    });
+  });
+});
